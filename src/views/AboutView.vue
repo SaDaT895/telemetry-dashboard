9394c0680714677d3b46c75d167df1fe95d3c217
Sadat Ahmad <sadatahmadofficial@gmail.com>
2023-06-02 20:44:55 +0200

    Vuetify Intellisense

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,5 +1,14 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
-  </div>
+  <v-container>
+    <v-row class="text-center">
+    <v-col>
+    <RouterLink to="/"><h1>Go back</h1></RouterLink>
+    <v-card>
+      <v-card-title primary-title>
+        BItch ass Card
+      </v-card-title>
+    </v-card>
+    </v-col>
+    </v-row>
+  </v-container>
 </template>
