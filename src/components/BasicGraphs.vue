d367e38bcf8bfae62ab44d39630c3407a7deb6e9
Sadat Ahmad <sadatahmadofficial@gmail.com>
2023-06-28 16:44:18 +0200

    Minor Changes

--- src/components/BasicGraphs.vue
+++ src/components/BasicGraphs.vue
@@ -1,118 +1,118 @@
 <template>
-    <div>
-      <p v-if="overlayId">Comparison with Lap {{ overlayId }}</p>
+  <div>
+    <h3 v-if="overlayId">Comparison with Lap {{ overlayId }} ({{ getLapTime(telemetry.data.lap.findLast(e => e.lap_count === overlayId)?.current_lap) }})</h3>
     <div style="height: 25vh;">
-              <line-chart :chart-data="speedData" :chart-options="{
-                  responsive: true,
-                  maintainAspectRatio: false,
-                  hover: {
-                    mode: 'index',
-                    intersect: false
-                  },
-                  onClick: (event, elements, chart) => {
-                    onHoverEmit(elements[0]?.element.$context.raw.timestamp)
-                  },
-                  scales: {
-                      x: {
-                          ticks: {
-                              callback(tickValue, index, ticks) {
-                                  if (tickValue as number % 500 === 0) return tickValue + 'm'
-                              },
-                          }
-                      }
-                  },
-                  plugins: {
-                      crosshair: {
-                          line: {
-                              color: 'white'
-                          },
-                          sync: {
-                            enabled: false
-                          },
-                          zoom: {
-                              enabled: false
-                          }
-                      }
-                  }
-              }"/>
-            </div>
-            <div style="height: 25vh;" class="my-5">
-               <line-chart :chart-data="gearData" :chart-options="{
-                   responsive: true,
-                   maintainAspectRatio: false,
-                   scales: {
-                       y: {
-                           ticks: {
-                               stepSize: 1
-                           }
-                       },
-                       x: {
-                           ticks: {
-                               callback(tickValue, index, ticks) {
-                                if (tickValue as number % 500 === 0) return tickValue + 'm'
-                               }
-                           }
-                       }
-                   },
-                   plugins: {
-                       crosshair: {
-                           line: {
-                               color: 'white'
-                           },
-                           sync: {
-                            enabled: false,
-                           },
-                           zoom: {
-                               enabled: false
-                           }
-                       }
-                   }
-               }"/>
-            </div>
-             <div style="height: 25vh;" class="my-5">
-                 <line-chart :chart-data="revData" :chart-options="{
-                     responsive: true,
-                     maintainAspectRatio: false,
-                     scales: {
-                         x: {
-                             ticks: {
-                                 callback(tickValue, index, ticks) {
-                                  if (tickValue as number % 500 === 0) return tickValue + 'm'
-                                 }
-                             }
-                         }
-                     },
-                     plugins: {
-                         crosshair: {
-                             line: {
-                                 color: 'white'
-                             },
-                             sync: {
-                                 enabled: false,
-                             },
-                             zoom: {
-                                 enabled: false
-                             }
-                         },
-                         zoom: {
-                          limits: {
-                            x: {min: 0}
-                          },
-                             pan: {
-                                 enabled: true,
-                                 mode: 'x',
-                             },
-                             zoom: {
-                                 wheel: {
-                                     enabled: true
-                                 },
-                                 mode: 'x'
-                             }
-                         }
-                     }
-                 }"/>
-            </div>
-            </div>
+      <line-chart :chart-data="speedData" :chart-options="{
+        responsive: true,
+        maintainAspectRatio: false,
+        hover: {
+          mode: 'index',
+          intersect: false
+        },
+        onClick: (event, elements, chart) => {
+          onHoverEmit(elements[0]?.element.$context.raw.timestamp)
+        },
+        scales: {
+          x: {
+            ticks: {
+              callback(tickValue, index, ticks) {
+                if (tickValue as number % 500 === 0) return tickValue + 'm'
+              },
+            }
+          }
+        },
+        plugins: {
+          crosshair: {
+            line: {
+              color: 'white'
+            },
+            sync: {
+              enabled: false
+            },
+            zoom: {
+              enabled: false
+            }
+          }
+        }
+      }" />
+    </div>
+    <div style="height: 25vh;" class="my-5">
+      <line-chart :chart-data="gearData" :chart-options="{
+        responsive: true,
+        maintainAspectRatio: false,
+        scales: {
+          y: {
+            ticks: {
+              stepSize: 1
+            }
+          },
+          x: {
+            ticks: {
+              callback(tickValue, index, ticks) {
+                if (tickValue as number % 500 === 0) return tickValue + 'm'
+              }
+            }
+          }
+        },
+        plugins: {
+          crosshair: {
+            line: {
+              color: 'white'
+            },
+            sync: {
+              enabled: false,
+            },
+            zoom: {
+              enabled: false
+            }
+          }
+        }
+      }" />
+    </div>
+    <div style="height: 25vh;" class="my-5">
+      <line-chart :chart-data="revData" :chart-options="{
+        responsive: true,
+        maintainAspectRatio: false,
+        scales: {
+          x: {
+            ticks: {
+              callback(tickValue, index, ticks) {
+                if (tickValue as number % 500 === 0) return tickValue + 'm'
+              }
+            }
+          }
+        },
+        plugins: {
+          crosshair: {
+            line: {
+              color: 'white'
+            },
+            sync: {
+              enabled: false,
+            },
+            zoom: {
+              enabled: false
+            }
+          },
+          zoom: {
+            limits: {
+              x: { min: 0 }
+            },
+            pan: {
+              enabled: true,
+              mode: 'x',
+            },
+            zoom: {
+              wheel: {
+                enabled: true
+              },
+              mode: 'x'
+            }
+          }
+        }
+      }" />
+    </div>
+  </div>
 </template>
 
 <script lang="ts">
@@ -120,7 +120,7 @@ import { telemetry } from '@/store'
 import { ChartData, ChartDataset } from 'chart.js'
 import { defineComponent } from 'vue'
 import LineChart from '@/components/LineChart.vue'
-import { getLapData, labels, trackLength } from '..'
+import { getLapData, getLapTime, labels, trackLength } from '..'
 
 export default defineComponent({
   name: 'BasicGraphs',
@@ -184,7 +184,6 @@ export default defineComponent({
             timestamp: v.timestamp
           }
         })
-        console.log(overlayData)
         datasets.push({
           data: overlayData,
           label: 'Speed ' + this.overlayId,
@@ -205,16 +204,37 @@ export default defineComponent({
           y: v.gear
         }
       })
+      const datasets: ChartDataset<'line'>[] = [
+        {
+          data: data,
+          label: 'Gear',
+          spanGaps: true,
+          ...this.dataOptions,
+          interpolate: true
+        }
+      ]
+      if (this.overlayId) {
+        const start = telemetry.data.car.findIndex(c => c.timestamp === this.overlayLapData![0].timestamp)
+        const end = telemetry.data.car.findIndex(c => c.timestamp === this.overlayLapData!.at(-1).timestamp)
+        const carData = telemetry.data.car.slice(start, end + 1).map(c => c.gear)
+        const overlayData = this.overlayLapData!.map((v, i) => {
+          return {
+            x: Math.round(v.lap_position * this.trackLength),
+            y: carData[i]
+          }
+        })
+        datasets.push({
+          data: overlayData,
+          label: 'Gear ' + this.overlayId,
+          ...this.dataOptions,
+          spanGaps: true,
+          backgroundColor: '#FF0000',
+          borderColor: '#FF0000'
+        })
+      }
       return {
         labels: this.labels,
-        datasets: [
-          {
-            data: data,
-            label: 'Gear',
-            spanGaps: true,
-            ...this.dataOptions
-          }
-        ]
+        datasets: datasets
       }
     },
     revData (): ChartData<'line'> {
@@ -224,15 +244,35 @@ export default defineComponent({
           y: v.rpm
         }
       })
+      const datasets: ChartDataset<'line'>[] = [
+        {
+          data: data,
+          label: 'RPM',
+          ...this.dataOptions,
+          interpolate: true
+        }
+      ]
+      if (this.overlayId) {
+        const start = telemetry.data.car.findIndex(c => c.timestamp === this.overlayLapData![0].timestamp)
+        const end = telemetry.data.car.findIndex(c => c.timestamp === this.overlayLapData!.at(-1).timestamp)
+        const carData = telemetry.data.car.slice(start, end + 1).map(c => c.rpm)
+        const overlayData = this.overlayLapData!.map((v, i) => {
+          return {
+            x: Math.round(v.lap_position * this.trackLength),
+            y: carData[i]
+          }
+        })
+        datasets.push({
+          data: overlayData,
+          label: 'RPM ' + this.overlayId,
+          ...this.dataOptions,
+          backgroundColor: '#FF0000',
+          borderColor: '#FF0000'
+        })
+      }
       return {
         labels: this.labels,
-        datasets: [
-          {
-            data: data,
-            label: 'RPM',
-            ...this.dataOptions
-          }
-        ]
+        datasets: datasets
       }
     }
   },
@@ -242,7 +282,8 @@ export default defineComponent({
   methods: {
     onHoverEmit (timestamp: any) {
       this.$emit('graphhover', timestamp)
-    }
+    },
+    getLapTime
   }
 })
 </script>
